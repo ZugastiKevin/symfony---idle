322202ff77ddd7b5d9b4c8a46eea7dc4c659c712
Kevin Zugasti <bearded-developer@protonmail.com>
2026-05-21 12:57:31 +0200

    Améliorer FactionChoiceType avec data-attributes et validations
    - Ajouter choice_attr pour passer la description en data-attribute
    - Ajouter contrainte de validation NotBlank
    - Corriger le template Twig pour utiliser les data-attributes
    - Utiliser faction_field.vars.label et faction_field.vars.attr

--- src/Form/FactionChoiceType.php
+++ src/Form/FactionChoiceType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class FactionChoiceType extends AbstractType
 {
@@ -18,9 +19,17 @@ class FactionChoiceType extends AbstractType
                 'label' => false,
                 'class' => Faction::class,
                 'choice_label' => 'name',
+                'choice_attr' => function (Faction $faction) {
+                    return [
+                        'data-description' => $faction->getDescription(),
+                    ];
+                },
                 'expanded' => true,
                 'multiple' => false,
                 'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(message: 'Veuillez choisir une faction.'),
+                ],
             ])
         ;
     }
