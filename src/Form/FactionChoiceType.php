751c9a98f95332e2322f989d7a274cce8519b25e
Kevin Zugasti <bearded-developer@protonmail.com>
2026-05-21 12:47:03 +0200

    Adapter logique et template pour utiliser l'entité Faction
    - Remplacer ChoiceType par EntityType dans FactionChoiceType
    - Utiliser l'entité Faction au lieu des constantes User::FACTION_CHOICES
    - Améliorer le template choose_faction.html.twig avec des cartes de faction
    - Ajouter CSS pour styliser les sélecteurs de faction
    - Afficher le nom et la description de chaque faction

--- src/Form/FactionChoiceType.php
+++ src/Form/FactionChoiceType.php
@@ -3,30 +3,24 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Faction;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 
 class FactionChoiceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('faction', ChoiceType::class, [
-                'label' => 'Choisissez votre faction',
-                'choices' => User::FACTION_CHOICES,
+            ->add('faction', EntityType::class, [
+                'label' => false,
+                'class' => Faction::class,
+                'choice_label' => 'name',
                 'expanded' => true,
                 'multiple' => false,
                 'required' => true,
-                'constraints' => [
-                    new Assert\NotBlank(message: 'Veuillez choisir une faction.'),
-                ],
-            ])
-            ->add('save', SubmitType::class, [
-                'label' => 'Valider mon choix',
             ])
         ;
     }
