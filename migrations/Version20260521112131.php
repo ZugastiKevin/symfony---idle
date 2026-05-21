4249c68a24235334f19adcd19bbd76b807d3ce76
Kevin Zugasti <bearded-developer@protonmail.com>
2026-05-21 13:22:31 +0200

    refrech migration

--- migrations/Version20260521112131.php
+++ migrations/Version20260521112131.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260519124358 extends AbstractMigration
+final class Version20260521112131 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,14 @@ final class Version20260519124358 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE building ADD chunk_id INT NOT NULL');
         $this->addSql('ALTER TABLE building ADD CONSTRAINT FK_E16F61D4F28401B9 FOREIGN KEY (building_type_id) REFERENCES building_type (id)');
         $this->addSql('ALTER TABLE building ADD CONSTRAINT FK_E16F61D4A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE building ADD CONSTRAINT FK_E16F61D4E48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
         $this->addSql('ALTER TABLE building ADD CONSTRAINT FK_E16F61D48BD543CD FOREIGN KEY (chunk_id) REFERENCES chunk (id)');
-        $this->addSql('CREATE INDEX IDX_E16F61D48BD543CD ON building (chunk_id)');
         $this->addSql('ALTER TABLE resource_deposit ADD CONSTRAINT FK_C87B7AD1962F8178 FOREIGN KEY (road_id) REFERENCES road (id)');
         $this->addSql('ALTER TABLE road ADD CONSTRAINT FK_95C0C4B18BD543CD FOREIGN KEY (chunk_id) REFERENCES chunk (id)');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649E48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6494448F8DA FOREIGN KEY (faction_id) REFERENCES faction (id)');
     }
 
     public function down(Schema $schema): void
@@ -38,10 +37,9 @@ final class Version20260519124358 extends AbstractMigration
         $this->addSql('ALTER TABLE building DROP FOREIGN KEY FK_E16F61D4A76ED395');
         $this->addSql('ALTER TABLE building DROP FOREIGN KEY FK_E16F61D4E48FD905');
         $this->addSql('ALTER TABLE building DROP FOREIGN KEY FK_E16F61D48BD543CD');
-        $this->addSql('DROP INDEX IDX_E16F61D48BD543CD ON building');
-        $this->addSql('ALTER TABLE building DROP chunk_id');
         $this->addSql('ALTER TABLE resource_deposit DROP FOREIGN KEY FK_C87B7AD1962F8178');
         $this->addSql('ALTER TABLE road DROP FOREIGN KEY FK_95C0C4B18BD543CD');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649E48FD905');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6494448F8DA');
     }
 }
